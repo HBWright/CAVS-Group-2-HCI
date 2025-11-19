d66e908436f553e27440e80f7ee8421d233abfb3
puckblocker <servergoalie0423@gmail.com>
2025-11-19 01:20:24 -0600

    Changes to Home Screen
    Added Cavs logo, edited buttons to change color when clicked

--- CavsUI/CavsUiReact/src/Home/HomePage.tsx
+++ CavsUI/CavsUiReact/src/Home/HomePage.tsx
@@ -1,31 +1,42 @@
 import { useNavigate } from "react-router-dom";
+import React, { useState } from "react";
 import styles from "./HomePageButtons.module.css";
+import logo from "../assets/LogoCavsHome.png";
 
 function Home() {
   const navigate = useNavigate();
+  const [activeButton, setActiveButton] = useState("");
+  const handleClick = (buttonName: string, path: string) => {
+    setActiveButton(buttonName);
+    navigate(path);
+  };
   console.log("Styles object:", styles);
 
   return (
     <div className={styles.HomePageContainer}>
+      <img src={logo} alt="CAVS Logo" className={styles.HomePageLogo}></img>
       <button
-        className={styles.ButtonOperator}
-        onClick={() => navigate("/operator")}
+        className={`${styles.ButtonOperator}
+        ${activeButton === "operator" ? styles.ButtonActive : ""}`}
+        onClick={() => handleClick("operator", "/operator")}
       >
         Operator
       </button>
       <button
-        className={styles.ButtonPassenger}
-        onClick={() => navigate("/passenger")}
+        className={`${styles.ButtonPassenger}
+        ${activeButton === "passenger" ? styles.ButtonActive : ""}`}
+        onClick={() => handleClick("passenger", "/passenger")}
       >
         Passenger
       </button>
       <button
-        className={styles.ButtonSponsor}
-        onClick={() => navigate("/sponsor")}
+        className={`${styles.ButtonSponsor}
+        ${activeButton === "sponsor" ? styles.ButtonActive : ""}`}
+        onClick={() => handleClick("sponsor", "/sponsor")}
       >
         Sponsor
       </button>
-      <h1 className={styles.HeadingHome}>CAVS</h1>
+      <h1 className={styles.HeadingHome}>Welcome on Board!</h1>
     </div>
   );
 }
